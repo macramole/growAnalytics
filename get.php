8a11c9c5d9232a6eda88a4ef996092de338f4aeb
leandro <leandrogarber@gmail.com>
2018-01-19 14:42:58 -0300

    mas

--- get.php
+++ get.php
@@ -2,6 +2,11 @@
 
 include_once("db.config.php");
 
+$queryTypes = []
+$queryTypes[0] = "created >= DATE_SUB(NOW(), INTERVAL 2 DAY) AND ( deviceID = 'temperaturaAfuera' OR deviceID = 'temperaturaAdentro' )";
+$queryTypes[1] = "created >= DATE_SUB(NOW(), INTERVAL 2 DAY) AND ( deviceID = 'humedadAfuera' OR deviceID = 'humedadAdentro' )";
+$queryTypes[2] = "created >= DATE_SUB(NOW(), INTERVAL 2 DAY) AND ( deviceID = 'humedadTierra1' OR deviceID = 'humedadTierra2' OR deviceID = 'humedadTierra3' )";
+
 $sensorData = DB::query("
 SELECT
 	*,
@@ -9,14 +14,7 @@ SELECT
 FROM
 	sensors
 WHERE
-	created >= DATE_SUB(NOW(), INTERVAL 2 DAY) AND
-	( deviceID = 'temperaturaAfuera' OR deviceID = 'temperaturaAdentro' )
-	");
-
-/*
--- WHERE
-	created BETWEEN DATE_SUB(NOW(), INTERVAL 1 DAY) AND NOW()
-	*/
+	" . $queryTypes[ $_GET["t"] ] );
 
 foreach( $sensorData[0] as $fieldName => $field ) {
 	echo "$fieldName\t";
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
 var lastValue = {};
+var lastChecked = false;
 
 // d3.tsv("getLast.php", type, function(error, tsvData) {
 //     if (error) throw error;
@@ -20,33 +21,47 @@ var lastValue = {};
 //     return d;
 // }
 
-d3.tsv("get.php", type, function(error, tsvData) {
-    if (error) throw error;
+function parseData( divGraph ) {
+    return function(error, tsvData) {
+        if (error) throw error;
 
-    data = [];
-    deviceIDs = [];
+        data = [];
+        deviceIDs = [];
 
-    for(row of tsvData) {
-        if ( deviceIDs.indexOf(row.deviceID) == -1 ) {
-            deviceIDs.push(row.deviceID);
+        for(row of tsvData) {
+            if ( deviceIDs.indexOf(row.deviceID) == -1 ) {
+                deviceIDs.push(row.deviceID);
+            }
+        };
+
+        for( deviceID of deviceIDs ) {
+            var dataByDeviceID = tsvData.filter(function(d) { return d.deviceID == deviceID });
+            data.push({
+                name : deviceID,
+                x : dataByDeviceID.map( function(d) { return d.date } ),
+                y : dataByDeviceID.map( function(d) { return d.value } ),
+                type : "scatter"
+            });
         }
-    };
 
-    for( deviceID of deviceIDs ) {
-        var dataByDeviceID = tsvData.filter(function(d) { return d.deviceID == deviceID });
-        data.push({
-            name : deviceID,
-            x : dataByDeviceID.map( function(d) { return d.date } ),
-            y : dataByDeviceID.map( function(d) { return d.value } ),
-            type : "scatter"
-        });
+        var layout = {
+            title : divGraph.substr(5)
+        };
+
+        Plotly.newPlot(divGraph, data, layout);
+
+        if ( !lastChecked ) {
+            divLastCheck = document.querySelector("#lastCheck span");
+            divLastCheck.innerHTML = data[0].x[ data[0].x.length - 1 ];
+            lastChecked = true;
+        }
     }
+}
 
-    Plotly.newPlot('graphTemperaturas', data);
+d3.tsv("get.php?t=0", type, parseData("graphTemperaturas") );
+d3.tsv("get.php?t=1", type, parseData("graphHumedades") );
+d3.tsv("get.php?t=2", type, parseData("graphTierras") );
 
-    divLastCheck = document.querySelector("#lastCheck span");
-    divLastCheck.innerHTML = data[0].x[ data[0].x.length - 1 ];
-});
 
 function type(d) {
     d.date = parseTime(d.createdb);
